4f27c97e6d85107bfea12ca60a2f20df6e0fa0e0
paine1690 <paine1690@126.com>
2016-03-04 16:12:45 +0800

    141. Linked List Cycle
    快慢两个指针

--- src/Solution.java
+++ src/Solution.java
@@ -611,6 +611,23 @@ public class Solution {
         node.next=node.next.next;
     }
     
+    public boolean hasCycle(ListNode head) {
+        if(head==null||head.next==null){
+        	return false;
+        }
+    	ListNode slow=head;
+    	ListNode fast=head;
+    	while(fast!=null&&fast.next!=null){
+    		slow=slow.next;
+    		fast=fast.next.next;
+    		if(slow==fast){
+    			return true;
+    		}
+    	}
+    	
+    	return false;
+    }
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String p="abba";
