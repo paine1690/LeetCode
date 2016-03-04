0e639d1f4cfb5026ba4eeee0d4e31de66b526f87
paine1690 <paine1690@126.com>
2016-03-04 16:11:53 +0800

    237. Delete Node in a Linked List
    两行代码

--- src/Solution.java
+++ src/Solution.java
@@ -605,7 +605,11 @@ public class Solution {
     	return re;
     }
     
-    
+    public void deleteNode(ListNode node) {
+    	
+        node.val=node.next.val;
+        node.next=node.next.next;
+    }
     
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
