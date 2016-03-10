de4a10ec4032f934ec44dd296f9f8ee7569219e2
paine1690 <paine1690@126.com>
2016-03-10 20:57:29 +0800

    88. Merge Sorted Array
    开始还以为会一次通过不然都对不起晓慧姐，没想到太大意了，要判断 很多条件。

--- src/TwoPointers.java
+++ src/TwoPointers.java
@@ -48,6 +48,33 @@ public class TwoPointers {
 		return true;
 	}
 	 
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+    	if(n==0){
+    		return;
+    	}
+    	int[] nums=new int[m+n];
+    	
+        int i=0, j=0, k=0;
+        while(i<m&&j<n){
+        	if(nums1[i]<nums2[j]){
+        		nums[k++]=nums1[i++];
+        	}else{
+        		nums[k++]=nums2[j++];
+        	}
+        }
+        while(i<m){
+        	nums[k++]=nums1[i++];
+        }
+        while(j<n){
+        	nums[k++]=nums2[j++];
+        }
+        for(i=0; i<m+n; i++){
+        	nums1[i]=nums[i];
+        }
+    }
+    
+    
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String s=".a";
