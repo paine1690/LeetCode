9c981182b66cbc582ac584256f4b05c03c386563
paine1690 <paine1690@126.com>
2016-03-10 21:02:20 +0800

    88. Merge Sorted Array
    看了一下网上的答案，从后向前比较，这样就不会覆盖原来的元素了，思维要活跃。
    千万不能大意

--- src/TwoPointers.java
+++ src/TwoPointers.java
@@ -52,24 +52,16 @@ public class TwoPointers {
     	if(n==0){
     		return;
     	}
-    	int[] nums=new int[m+n];
-    	
-        int i=0, j=0, k=0;
-        while(i<m&&j<n){
-        	if(nums1[i]<nums2[j]){
-        		nums[k++]=nums1[i++];
+        int i=m-1, j=n-1, k=m+n-1;
+        while(i>=0&&j>=0){
+        	if(nums1[i]>nums2[j]){
+        		nums1[k--]=nums1[i--];
         	}else{
-        		nums[k++]=nums2[j++];
+        		nums1[k--]=nums2[j--];
         	}
         }
-        while(i<m){
-        	nums[k++]=nums1[i++];
-        }
-        while(j<n){
-        	nums[k++]=nums2[j++];
-        }
-        for(i=0; i<m+n; i++){
-        	nums1[i]=nums[i];
+        while(j>=0){
+        	nums1[k--]=nums2[j--];
         }
     }
     
