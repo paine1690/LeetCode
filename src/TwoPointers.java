7ed523f246d36acc0942f9d485e72290a3b5e076
paine1690 <paine1690@126.com>
2016-03-10 21:38:22 +0800

    28. Implement strStr()
    想出的复杂度是O(m*n)，这类问题有一个KMP算法，可以研究一下

--- src/TwoPointers.java
+++ src/TwoPointers.java
@@ -65,12 +65,33 @@ public class TwoPointers {
         }
     }
     
+    public static int strStr(String haystack, String needle) {
+    	if(needle.length()==0){
+    		return 0;
+    	}
+    	if(haystack.length()==0){
+    		return -1;
+    	}
+    	int i, j;
+    	for(i=0; i<haystack.length()-needle.length()+1;i++){
+			for(j=0; j<needle.length(); j++){
+				if(haystack.charAt(i+j)!=needle.charAt(j)){
+					break;
+				}
+			}
+			if(j==needle.length()){
+				return i;
+			}
+    	}
+        return -1;
+    }
     
     
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s=".a";
-		System.out.println(isPalindrome2(s));
+		String s1="asdf";
+		String s2="df";
+		System.out.println(strStr(s1, s2));
 	}
 
 }
