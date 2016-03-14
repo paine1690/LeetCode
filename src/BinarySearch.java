c84442f6cfa767d369a66da595eb636223389271
paine1690 <paine1690@126.com>
2016-03-14 19:55:54 +0800

    69. Sqrt(x)
    求积的时候也要考虑溢出情况

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -18,9 +18,30 @@ public class BinarySearch {
     	return left;
     }
     
+    public static int mySqrt(int x) {
+    	if(x<2){
+    		return x;
+    	}
+        int left=1, right=x/2+1;
+    	while(left<=right){
+    		int mid=left+(right-left)/2;
+    		int temp=x/mid;
+    		if(temp==mid){
+    			return mid;
+    		}else if(temp<mid){
+    			right=mid-1;
+    		}else{
+    			left=mid+1;
+    		}
+    	}
+    	return right;
+    }
+    
+    
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		System.out.println(mySqrt(15));
 
 	}
 
