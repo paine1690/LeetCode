888efd961cd8212bb358c87ac92bfc62b5e88448
paine1690 <paine1690@126.com>
2016-03-14 21:13:54 +0800

    34. Search for a Range

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class BinarySearch {
 	//没有具体功能，为了确保编译没有错误。
@@ -37,11 +38,35 @@ public class BinarySearch {
     	return right;
     }
     
-    
+    public static int[] searchRange(int[] nums, int target) {
+        int left=0, right=nums.length-1;
+        int[] re={-1,-1};
+        while(left<=right){
+        	int mid=left+(right-left)/2;
+        	if(nums[mid]==target){
+                int i=mid, j=mid;
+                while(j<nums.length-1&&nums[j+1]==target){
+                	j++;
+                }
+                while(i>0&&nums[i-1]==target){
+                	i--;
+                }
+                re[0]=i;       	
+                re[1]=j;	
+        		break;
+        	}else if(nums[mid]<target){
+        		left=mid+1;
+        	}else{
+        		right=mid-1;
+        	}
+        }
+        return re;
+    }
     
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(mySqrt(15));
+		int[] nums={5,7,7,8,8,10};
+		System.out.println(Arrays.toString(searchRange(nums, 8)));
 
 	}
 
