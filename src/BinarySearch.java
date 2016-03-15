95c182f2df9ebe1be908a373ab20853746e96117
paine1690 <paine1690@126.com>
2016-03-15 14:36:16 +0800

    35. Search Insert Position
    现在好像搞明白了
    怪不得通过率这么高，确实不难

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -6,17 +6,17 @@ public class BinarySearch {
 		return true;
 	}
 	
-    public int firstBadVersion(int n) {
+	public int firstBadVersion(int n) {
     	int left=1, right=n;
-        while(left<right){
+        while(left<=right){
         	int mid=left+(right-left)/2;
         	if(isBadVersion(mid)){
-        		right=mid;
+        		right=mid-1;
         	}else{
         		left=mid+1;
         	}
         }    	
-    	return left;
+    	return right+1;
     }
     
     public static int mySqrt(int x) {
@@ -100,13 +100,28 @@ public class BinarySearch {
     	return false;
     }
    
+    public static int searchInsert(int[] nums, int target) {
+        int mid, left=0, right=nums.length-1;
+        while(left<=right){
+        	mid=left+(right-left)/2;
+        	if(nums[mid]==target){
+        		return mid;
+        	}else if(nums[mid]>target){
+        		right=mid-1;
+        	}else{
+        		left=mid+1;
+        	}
+        }
+        
+        return right+1;
+    }
     
     
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] nums={5,7,7,8,8,10};
+		int[] nums={1,3,5,6};
 		int[][] matrix={{1,3}};
-		System.out.println(searchMatrix(matrix, 3));
+		System.out.println(searchInsert(nums, 2));
 
 	}
 
