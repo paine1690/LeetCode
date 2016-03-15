5ce13674cfedf61959746f23969580492849db11
paine1690 <paine1690@126.com>
2016-03-15 14:20:38 +0800

    74. Search a 2D Matrix
    对于返回第一个和最后一个 还是没太明白 在多写写

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -66,10 +66,47 @@ public class BinarySearch {
         return re;
     }
     
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int m=matrix.length;
+        int n=matrix[0].length;
+    	int mid = 0, left=0, right=m-1;
+    	while(left<=right){
+    		mid=left+(right-left)/2;
+    		if(matrix[mid][0]==target){
+    			return true;
+    		}else if(matrix[mid][0]>target){
+    			right=mid-1;
+    		}else{
+    			left=mid+1;
+    		}
+    	}
+    	if(right==-1){
+    		return false;
+    	}
+    	int temp=right;
+    	left=0; 
+    	right=n-1;
+    	while(left<=right){
+    		mid=left+(right-left)/2;
+    		if(matrix[temp][mid]==target){
+    			return true;
+    		}else if(matrix[temp][mid]<target){
+    			left=mid+1;
+    		}else{
+    			right=mid-1;
+    		}
+    	}
+    	return false;
+    }
+   
+    
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] nums={5,7,7,8,8,10};
-		System.out.println(Arrays.toString(searchRange(nums, 8)));
+		int[][] matrix={{1,3}};
+		System.out.println(searchMatrix(matrix, 3));
 
 	}
 
