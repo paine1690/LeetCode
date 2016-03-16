04397b544031991e1b122748f30c8296f86107b6
paine1690 <paine1690@126.com>
2016-03-16 21:01:00 +0800

    274. H-Index
    运用计数排序，O(n)。
    数组B{i},j记录引用次数大于i的个数。
    i最大使得B[i]>=1，

--- src/HashTable.java
+++ src/HashTable.java
@@ -4,23 +4,12 @@ public class HashTable {
 	/*
 	 * 类名HashTable是按照leetcode上面的tag起的，实际用的是HashMap
 	 */
-	public static int hIndex(int[] citations) {
-        Arrays.sort(citations);
-        int len=citations.length;
-        int i=0;
-        for(i=1; i<=len; i++){
-        	if(citations[len-i]<i){
-        		break;
-        	}
-        }
-		return i-1;
-    }
+	
 	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] nums={0,0,0,0,0,0};
-		System.out.println(hIndex(nums));
+		
 	}
 
 }
---
+++ src/Sort.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public class Sort {
+	//O(logn)
+	public static int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int len=citations.length;
+        int i=0;
+        for(i=1; i<=len; i++){
+        	if(citations[len-i]<i){
+        		break;
+        	}
+        }
+		return i-1;
+    }
+	//O(n)
+	public static int hIndex2(int[] citations) {
+		int len=citations.length;
+		int[] B=new int[len+1];
+		for(int i=0; i<len; i++){
+			if(citations[i]>len){
+				B[len]++;
+			}else{
+				B[citations[i]]++;
+			}
+		}
+		for(int i=len-1; i>=0; i--){
+			B[i]=B[i]+B[i+1];
+		}
+		for(int i=len; i>=0; i--){
+			if(B[i]>=i){
+				return i;
+			}
+		}		
+		return 0;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] nums={3, 0, 6, 1, 5};
+		System.out.println(hIndex2(nums));
+	}
+
+}
