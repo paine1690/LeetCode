951f42f30e0f8509f86abc1ffe35e6dee4898ffc
paine1690 <paine1690@126.com>
2016-03-21 15:37:19 +0800

    205. Isomorphic Strings
    好慢

--- src/HashTable.java
+++ src/HashTable.java
@@ -19,10 +19,35 @@ public class HashTable {
     	return it.next();
     }
 	
+    public static boolean isIsomorphic(String s, String t) {
+    	if(s.length()<2){
+    		return true;
+    	}
+    	
+    	Map<Character, Integer> map1=new HashMap<Character, Integer>();
+    	Map<Integer, Character> map2=new HashMap<Integer, Character>();
+    	for(int i=0; i<s.length(); i++){
+    		if(!map1.containsKey(s.charAt(i))){
+    			if(map2.containsValue(t.charAt(i))){
+    				return false;
+    			}else{
+    				map1.put(s.charAt(i), i);
+    				map2.put(i, t.charAt(i));
+    			}
+    		}else{
+    			if(!map2.get(map1.get(s.charAt(i))).equals(t.charAt(i))){
+    				return false;
+    			}
+    		}
+    	}
+    	return true;
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] nums={1,5,6,4,1,6,4};
-		System.out.println(singleNumber(nums));
+		String s="ab";
+		String t="aa";
+		System.out.println(isIsomorphic(s, t));
 		
 	}
 
