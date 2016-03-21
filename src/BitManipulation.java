121a5dfe7908fbdd6a397d39c660dbdb0b2c24f8
paine1690 <paine1690@126.com>
2016-03-21 14:59:22 +0800

    136. Single Number
    太慢了 而且空间也不满足要求

---
+++ src/BitManipulation.java
@@ -0,0 +1,11 @@
+
+public class BitManipulation {
+	/*
+	 * 136.Single Number
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/HashTable.java
+++ src/HashTable.java
@@ -1,14 +1,28 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class HashTable {
 	/*
 	 * 类名HashTable是按照leetcode上面的tag起的，实际用的是HashMap
 	 */
 	
-	
+    public static int singleNumber(int[] nums) {
+    	Set<Integer> set=new HashSet<Integer>();
+    	for(int i=0; i<nums.length; i++){
+    		if(set.contains(nums[i])){
+    			set.remove(nums[i]);
+    		}else{
+    			set.add(nums[i]);
+    		}
+    	}
+    	
+    	Iterator<Integer> it=set.iterator();
+    	return it.next();
+    }
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] nums={1,5,6,4,1,6,4};
+		System.out.println(singleNumber(nums));
 		
 	}
 
