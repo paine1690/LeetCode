a80095c37ad994863082117932d54a964af7ba7c
paine1690 <paine1690@126.com>
2016-03-29 14:54:43 +0800

    338. Counting Bits
    没有用位运算

--- src/BitManipulation.java
+++ src/BitManipulation.java
@@ -1,11 +1,33 @@
+import java.util.Arrays;
 
 public class BitManipulation {
 	/*
 	 * 136.Single Number
 	 */
+	
+	public static int[] countBits(int num) {
+        int[] re=new int[num+1];
+        re[0]=0;
+        if(num==0){
+        	return re;
+        }
+        re[1]=1;
+        int step=2;
+        int i=2;
+        while(i<=num){
+        	for(int j=0; j<step&&i<=num; j++){
+        		re[i++]=re[j]+1;
+        	}
+        	step=step*2;
+        }
+        return re;
+    }
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(Arrays.toString(countBits(10)));
 	}
 
 }
