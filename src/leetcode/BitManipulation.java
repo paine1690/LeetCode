f1919a1c0c270cb53d83b927ec276104c36b8c4a
paine1690 <paine1690@126.com>
2016-03-31 17:47:10 +0800

    231. Power of Two

--- src/leetcode/BitManipulation.java
+++ src/leetcode/BitManipulation.java
@@ -115,6 +115,9 @@ public class BitManipulation {
 		return re;
 	}
 	
+    public static boolean isPowerOfTwo(int n) {
+    	return (n&(n-1))==0&&n!=0;
+    }
 	public static void main(String[] args) {
 		int[] nums={1,2,3,4,5,6,7};
 		
