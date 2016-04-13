4c5d5718a6fb4dbdbb981213a23289e927ee696f
paine1690 <paine1690@126.com>
2016-04-13 10:38:52 +0800

    //4. Median of Two Sorted Arrays
    分治法 很好的题

--- src/leetcode/Array_leetcode.java
+++ src/leetcode/Array_leetcode.java
@@ -1,5 +1,4 @@
 package leetcode;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 
@@ -7,52 +6,7 @@ public class Array_leetcode {
 
 	
     
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-    	int m=nums1.length;
-    	int n=nums2.length;
-        int mid=(m+n)/2;
-        int[] temp=new int[m+n];
-        double re=0;
-        if(m==0){
-        	for(int i=0;i<mid+1;i++){
-        		temp[i]=nums2[i];
-        	}
-        }
-        else if(n==0){
-        	for(int i=0;i<mid+1;i++){
-        		temp[i]=nums1[i];
-        	}
-        }
-        
-        else{
-            int i=0;
-            int j=0;
-            int k=0;
-            while(i<m&&j<n&&k<mid+1){
-                if(nums1[i]<nums2[j]){
-                    temp[k++]=nums1[i++];
-                }
-                else{
-                    temp[k++]=nums2[j++];
-                }
-            }
-            while(i<m&&k<mid+1){
-                temp[k++]=nums1[i++];
-            }
-            while(j<n&&k<mid+1){
-                temp[k++]=nums2[j++];
-            }
-            
-        }
-        if((m+n)%2==1){
-        	re=temp[mid];
-        }
-        else{
-        	re=(temp[mid]+temp[mid-1])/2.0;
-        }
-        
-        return re;    
-    }
+    
     
     public static boolean containsDuplicate(int[] nums) {
         HashSet<Integer> set=new HashSet<Integer>();
@@ -90,7 +44,7 @@ public class Array_leetcode {
     
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] aa={1,1,1,1,2,3};
+		//int[] aa={1,1,1,1,2,3};
 	}
 
 }
--- src/leetcode/DivideAndConquer.java
+++ src/leetcode/DivideAndConquer.java
@@ -1,7 +1,6 @@
 package leetcode;
 
 import java.util.Random;
-
 public class DivideAndConquer {
 	
 	//215. Kth Largest Element in an Array
@@ -50,10 +49,45 @@ public class DivideAndConquer {
 		}
 		return nums[r];		
 	}
+	
+	//4. Median of Two Sorted Arrays
+	private static double findKth(int[] nums1, int s1, int len1, int[] nums2, int s2, int len2, int k){
+		if(len1>len2){//保证nums1为短的那个
+			return findKth(nums2, s2, len2, nums1, s1, len1, k);
+		}
+		if(len1==0){//短数组中元素都被抛弃，返回长数组中第k个元素即nums2[s2+k-1]
+			return nums2[s2+k-1];
+		}
+		
+		if(k==1){
+			return java.lang.Math.min(nums1[s1], nums2[s2]);
+		}
+		int p=java.lang.Math.min(k/2, len1);
+		int q=k-p;
+		if(nums1[s1+p-1]<nums2[s2+q-1]){
+			return findKth(nums1, s1+p, len1-p, nums2, s2, len2, k-p);
+		}else if(nums1[s1+p-1]>nums2[s2+q-1]){
+			return findKth(nums1, s1, len1, nums2, s2+q, len2-q, k-q);
+		}else{
+			return nums1[s1+p-1];
+		}
+	}
+	
+	public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+		int k=nums1.length+nums2.length;
+		if((k&1)==1){
+			System.out.println("ji");
+			return findKth(nums1, 0, nums1.length, nums2, 0, nums2.length, k/2+1);
+		}else{
+			return (findKth(nums1, 0, nums1.length, nums2, 0, nums2.length, k/2)+findKth(nums1, 0, nums1.length, nums2, 0, nums2.length, k/2+1))/2;
+		}
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] nums={2,2};
-		System.out.println(findKthLargest(nums, 1));
+		int[] nums1={1,2};
+		int[] nums2={1,2};
+		System.out.println(findMedianSortedArrays(nums1, nums2));
 	}
 
 }
