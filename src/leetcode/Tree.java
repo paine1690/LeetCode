c95918145ad8bfc16c5dfc4af2712ab6938c124c
paine1690 <paine1690@126.com>
2016-04-14 10:30:45 +0800

    100. Same Tree
    前三道全是递归，如果非递归 应该用层序遍历

--- src/leetcode/Tree.java
+++ src/leetcode/Tree.java
@@ -28,7 +28,18 @@ public class Tree {
         return root;
     }
     
-    
+    //100. Same Tree
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+    	if(p==null&&q==null){
+    		return true;
+    	}
+    	if((p!=null&&q!=null)&&(p.val==q.val)){
+            return (isSameTree(p.left, q.left))&&(isSameTree(p.right, q.right));
+    	}
+        else{
+        	return false;
+        }
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
