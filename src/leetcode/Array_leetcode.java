5b8c71f50dc8eed9cb9c370a5f81ea0905c49dce
paine1690 <paine1690@126.com>
2016-05-09 14:45:36 +0800

    27. Remove Element

--- src/leetcode/Array_leetcode.java
+++ src/leetcode/Array_leetcode.java
@@ -2,10 +2,22 @@ package leetcode;
 
 public class Array_leetcode {
     
-	
+	//27. Remove Element
+    public static int removeElement(int[] nums, int val) {
+        int re=0;
+        for(int i=0; i<nums.length; i++){
+        	if(nums[i]==val){
+        		re++;
+        		continue;
+        	}
+        	nums[i-re]=nums[i];
+        }
+        return nums.length-re;
+    }
     
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] nums={3,2,2,3};
+		System.out.println(removeElement(nums, 3));
 		
 	}
 
