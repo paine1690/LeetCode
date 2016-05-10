750f2b4901be9cacdc03fb8ffcc08e9ab6143cbc
paine1690 <paine1690@126.com>
2016-05-10 14:50:43 +0800

    343. Integer Break
    找规律 比动态规划简单

--- src/leetcode/Math_leetcode.java
+++ src/leetcode/Math_leetcode.java
@@ -251,8 +251,23 @@ public class Math_leetcode {
     	}    	
     	return result;
     }
+    
+    //343. Integer Break
+    public static int integerBreak(int n) {
+        if(n<4){
+        	return n-1;
+        }
+        int re=1;
+        while(n>4){
+        	re*=3;
+        	n-=3;
+        }
+        return re*n;
+    }
+    
+    
 	public static void main(String[] args) {
-		System.out.println(multiply("1", "1"));
+		System.out.println(integerBreak(5));
 
 	}
 
