f7862c89fb176f422247ac167300b36e850c7483
paine1690 <paine1690@126.com>
2016-05-11 14:47:22 +0800

    64. Minimum Path Sum
    居然没看答案一次写出来了

--- src/leetcode/DynamicProgramming.java
+++ src/leetcode/DynamicProgramming.java
@@ -60,6 +60,32 @@ public class DynamicProgramming {
         }
     }
     
+    //64. Minimum Path Sum
+    public int minPathSum(int[][] grid) {
+    	if(grid.length==0||grid[0].length==0){
+        	return 0;
+        }
+        int m=grid.length;
+        int n=grid[0].length;
+        for(int i=1; i<m; i++){
+        	grid[i][0]+=grid[i-1][0];
+        }
+        for(int i=1; i<n; i++){
+        	grid[0][i]+=grid[0][i-1];
+        }
+        for(int i=1; i<m&&i<n; i++){
+        	grid[i][i]+=Math.min(grid[i-1][i], grid[i][i-1]);
+        	for(int j=i+1; j<m; j++){
+        		grid[j][i]+=Math.min(grid[j-1][i], grid[j][i-1]);
+        	}
+        	for(int j=i+1; j<n; j++){
+        		grid[i][j]+=Math.min(grid[i-1][j], grid[i][j-1]);
+        	}
+        }
+        return grid[m-1][n-1];
+    }
+    
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
