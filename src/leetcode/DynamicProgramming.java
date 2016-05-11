5baf08b33419ce5b235d1f3f93a328edd83a77e3
paine1690 <paine1690@126.com>
2016-05-11 19:29:29 +0800

    62. Unique Paths
    计算顺序可以优化

--- src/leetcode/DynamicProgramming.java
+++ src/leetcode/DynamicProgramming.java
@@ -98,12 +98,8 @@ public class DynamicProgramming {
     	for(int i=1; i<n; i++){
     		nums[0][i]=1;
     	}
-    	for(int i=1; i<m&&i<n; i++){
-    		nums[i][i]=nums[i][i-1]+nums[i-1][i];
-    		for(int j=i+1; j<m; j++){
-    			nums[j][i]=nums[j-1][i]+nums[j][i-1];
-    		}
-    		for(int j=i+1; j<n; j++){
+    	for(int i=1; i<m; i++){
+    		for(int j=1; j<n; j++){
     			nums[i][j]=nums[i-1][j]+nums[i][j-1];
     		}
     	}
