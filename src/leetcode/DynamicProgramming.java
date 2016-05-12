ed01310b33fc979a9c4ad9d24479849b62cf8c04
paine1690 <paine1690@126.com>
2016-05-12 15:29:07 +0800

    304. Range Sum Query 2D - Immutable
    和303差不多

--- src/leetcode/DynamicProgramming.java
+++ src/leetcode/DynamicProgramming.java
@@ -60,6 +60,42 @@ public class DynamicProgramming {
         }
     }
     
+    //304. Range Sum Query 2D - Immutable
+    class NumMatrix {
+    	private int[][] matrix;
+        public NumMatrix(int[][] matrix) {
+            this.matrix=matrix;
+            if(matrix.length==0||matrix[0].length==0){
+            	return;
+            }
+            int m=matrix.length;
+            int n=matrix[0].length;
+            
+            for(int i=1; i<n; i++){
+            	matrix[0][i]+=matrix[0][i-1];
+            }
+            
+            for(int i=1; i<m; i++){
+            	matrix[i][0]+=matrix[i-1][0];
+            	for(int j=1; j<n; j++){
+            		matrix[i][j]+=(matrix[i-1][j]+matrix[i][j-1]-matrix[i-1][j-1]);
+            	}
+            }            
+        }
+
+        public int sumRegion(int row1, int col1, int row2, int col2) {
+        	if(row1==0&&col1==0){
+        		return matrix[row2][col2];
+        	}else if(row1==0){
+        		return matrix[row2][col2]-matrix[row2][col1-1];
+        	}else if(col1==0){
+        		return matrix[row2][col2]-matrix[row1-1][col2];
+        	}else{
+        		return matrix[row2][col2]-matrix[row1-1][col2]-matrix[row2][col1-1]+matrix[row1-1][col1-1];
+        	}
+        }
+    }
+    
     //64. Minimum Path Sum
     public int minPathSum(int[][] grid) {
     	if(grid.length==0||grid[0].length==0){
@@ -113,6 +149,7 @@ public class DynamicProgramming {
         }
     	int m=obstacleGrid.length;
     	int n=obstacleGrid[0].length;
+    	obstacleGrid[0][0]=1;
     	int i=0;
     	for(i=1; i<m; i++){
     		if(obstacleGrid[i][0]==1){
@@ -146,6 +183,7 @@ public class DynamicProgramming {
     	}
     	return obstacleGrid[m-1][n-1];
     }
+    
     
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
