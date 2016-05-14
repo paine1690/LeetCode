b158e9c54286400658ba9dea5a7b1d0875a90d9e
paine1690 <paine1690@126.com>
2016-05-14 10:34:00 +0800

    96. Unique Binary Search Trees

--- src/leetcode/DynamicProgramming.java
+++ src/leetcode/DynamicProgramming.java
@@ -309,6 +309,23 @@ public class DynamicProgramming {
     	System.out.println(dp[nums.length-1]);
     	return Math.max(max, dp[nums.length-1]);
     }
+    
+    //96. Unique Binary Search Trees
+    public int numTrees(int n) {
+    	if(n<3){
+    		return n;
+    	}
+        int[] dp=new int[n+1];
+    	dp[0]=1;
+    	dp[1]=1;
+    	dp[2]=2;
+    	for(int i=3; i<=n; i++){
+    		for(int j=0; j<i; j++){
+    			dp[i]+=dp[j]*dp[i-j-1];
+    		}
+    	}
+    	return dp[n];
+    }
 	public static void main(String[] args) {
 		int nums[]={1,2,1,1};
 		//String s="1";
