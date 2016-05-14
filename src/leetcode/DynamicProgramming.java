de45cd6c34addaa92ad948b17ba87ca4b39df1b7
paine1690 <paine1690@126.com>
2016-05-14 09:52:08 +0800

    213. House Robber II
    第一个和最后一个不能同时  所以要求两趟

--- src/leetcode/DynamicProgramming.java
+++ src/leetcode/DynamicProgramming.java
@@ -283,10 +283,36 @@ public class DynamicProgramming {
         return nums[nums.length-1];
     }
     
+    //213. House Robber II
+    public static int rob2(int[] nums) {
+        if(nums.length<3){
+        	if(nums.length==2){
+            	return Math.max(nums[0], nums[1]);
+            }
+        	return nums.length==0? 0:nums[0];
+        }
+        
+        int max=0;
+        int[] dp=new int[nums.length];
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0], nums[1]);
+        
+    	for(int i=2; i<nums.length-1; i++){
+    		dp[i]=Math.max(dp[i-1], nums[i]+dp[i-2]);
+    	}
+    	max=dp[nums.length-2];
+    	dp[1]=nums[1];
+    	dp[2]=Math.max(nums[1], nums[2]);
+    	for(int i=3; i<nums.length; i++){
+    		dp[i]=Math.max(dp[i-1], nums[i]+dp[i-2]);
+    	}
+    	System.out.println(dp[nums.length-1]);
+    	return Math.max(max, dp[nums.length-1]);
+    }
 	public static void main(String[] args) {
-		//int nums[]={10, 9, 2, 5, 3, 7, 101, 18};
-		String s="1";
-		System.out.println(numDecodings(s));
+		int nums[]={1,2,1,1};
+		//String s="1";
+		System.out.println(rob2(nums));
 	}
 
 }
