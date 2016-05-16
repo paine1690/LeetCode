464b6e14070b07bdb15f62e2bf8acccd21f901c7
paine1690 <paine1690@126.com>
2016-05-16 15:18:32 +0800

    279. Perfect Squares
    好他妈难啊

--- src/leetcode/DynamicProgramming.java
+++ src/leetcode/DynamicProgramming.java
@@ -393,10 +393,25 @@ public class DynamicProgramming {
     	return max;
     }
     
+    //279. Perfect Squares
+    public int numSquares(int n) {
+        int[] dp=new int[n+1];
+    	Arrays.fill(dp, Integer.MAX_VALUE);
+    	for(int i=1; i*i<=n; i++){
+    		dp[i*i]=1;
+    	}
+    	for(int a=1; a<=n; a++){
+    		for(int b=1; a+b*b<=n; b++){
+    			dp[a+b*b]=Math.min(dp[a+b*b], dp[a]+1);
+    		}
+    	}
+    	return dp[n];
+    }
+    
 	public static void main(String[] args) {
-		int nums[]={3,-2,-3,4,-2};
+		//int nums[]={2};
 		//String s="1";
-		System.out.println(maxProduct(nums));
+		
 	}
 
 }
