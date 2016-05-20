fb2a003ed35c0e0cd30de5bb9458f8986f9343d1
paine1690 <paine1690@126.com>
2016-05-20 21:06:21 +0800

    292. Nim Game

---
+++ src/leetcode/Brainteaser.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+public class Brainteaser {
+
+	//292. Nim Game
+    public boolean canWinNim(int n) {
+        return n%4==0;
+    }
+    
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
