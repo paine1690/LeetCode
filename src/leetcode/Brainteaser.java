c04f23e0a90c7e46d9daa5de1ceef70a951248ab
paine1690 <paine1690@126.com>
2016-05-21 16:01:58 +0800

    319. Bulb Switcher

--- src/leetcode/Brainteaser.java
+++ src/leetcode/Brainteaser.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 public class Brainteaser {
 
 	//292. Nim Game
@@ -7,10 +9,32 @@ public class Brainteaser {
         return n%4==0;
     }
     
+    //319. Bulb Switcher
+    public static int bulbSwitch(int n) {
+//    	LTE
+//        int[] d=new int[n+1];
+//        for(int i=1; i<=n; i++){
+//        	for(int j=1; i*j<=n; j++){
+//        		d[i*j]++;
+//        	}
+//        }
+//        int re=0;
+//        System.out.println(Arrays.toString(d));
+//        for(int i=1; i<=n; i++){
+//        	if((d[i]&1)==1){
+//        		re++;
+//        	}
+//        }
+//        return re;
+    	return (int) Math.sqrt(n);
+    }
+    
+    
     
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(Math.sqrt(24));
+		System.out.println(bulbSwitch(24));
 	}
 
 }
