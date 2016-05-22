de18d52496be94f3ed5f0ebf118fad486d2d6f76
paine1690 <paine1690@126.com>
2016-05-22 20:21:38 +0800

    二分查找

--- src/second_leetcode/BinarySearch.java
+++ src/second_leetcode/BinarySearch.java
@@ -30,12 +30,34 @@ public class BinarySearch {
         return start;
     }
 	
-	
-	
+	/**
+	 * 返回第一个小于某数的序号
+	 */
+    
+    
+    
+    
+    
+    //35. Search Insert Position
+    public static int searchInsert(int[] nums, int target) {
+    	int start=0, end=nums.length-1;
+    	while(start<=end){
+    		int mid=(start+end)>>>1;
+    		if(nums[mid]==target){
+    			return mid;
+    		}else if(nums[mid]>target){
+    			end=mid-1;
+    		}else{
+    			start=mid+1;
+    		}
+    	}
+    	return start;
+    }
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] nums={1,3,5,6};
+		System.out.println(searchInsert(nums, 0));
 	}
 
 }
