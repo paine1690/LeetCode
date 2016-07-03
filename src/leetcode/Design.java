3a0ba8400ff8477d916744cb4972b4b236840f93
paine1690 <paine1690@126.com>
2016-07-03 11:03:35 +0800

    211. Add and Search Word - Data structure design

--- src/leetcode/Design.java
+++ src/leetcode/Design.java
@@ -1,9 +1,10 @@
 package leetcode;
 
+import java.util.Collection;
 import java.util.HashMap;
 
 public class Design {
-
+	//208. Implement Trie (Prefix Tree)
 	class TrieNode {
 		boolean isWord;
 		HashMap<Character, TrieNode> nexts;
@@ -82,10 +83,79 @@ public class Design {
 	}
 	
 	
+	//211. Add and Search Word - Data structure design
+	class WordDictionary {
+		private TrieNode root;
+		public WordDictionary(){
+			root=new TrieNode();
+		}
+	    // Adds a word into the data structure.
+	    public void addWord(String word) {
+	        TrieNode pNode=root;
+	        int i=0, len=word.length();
+	        
+	        while(i<len){
+	        	TrieNode next=pNode.nexts.get(word.charAt(i));
+	        	if(next==null){
+	        		break;
+	        	}
+	        	pNode=next;
+	        	i++;
+	        }
+	        
+	        while(i<len){
+	        	TrieNode newNode=new TrieNode();
+	        	pNode.nexts.put(word.charAt(i), newNode);
+	        	pNode=newNode;
+	        	i++;
+	        }
+	        
+	        pNode.isWord=true;
+	    }
+
+	    // Returns if the word is in the data structure. A word could
+	    // contain the dot character '.' to represent any one letter.
+	    public boolean search(String word) {
+	    	return searchFrom(root, word, 0);
+	    }
+	    
+	    private boolean searchFrom(TrieNode node, String word, int index){
+	    	if(index==word.length()){
+	    		System.out.println("fsd");
+	    		return node.isWord;
+	    	}
+	    	
+	        TrieNode pNode=node;
+	        if(word.charAt(index)!='.'){
+	        	TrieNode next=pNode.nexts.get(word.charAt(index));
+	        	if(next==null){
+	        		return false;
+	        	}else{
+	        		return searchFrom(next, word, index+1);
+	        	}
+	        }else{
+	        	boolean re=false;
+	        	java.util.Collection<TrieNode> coll=pNode.nexts.values();
+	        	for(TrieNode tNode:coll){
+	        		re|=searchFrom(tNode, word, index+1);
+	        	}
+	        	return re;
+	        }        
+	    }
+	}
+	
+	public Design(){
+		System.out.println("fsd");
+		WordDictionary dic=new WordDictionary();
+		dic.addWord("a");
+		dic.addWord("ab");
+		//System.out.println(dic.root.nexts.get('a').nexts.get('b').isWord);
+		System.out.println(dic.search("ab"));
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Design d=new Design();
 	}
 
 }
