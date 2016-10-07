2a0713e6b3813f4dbaf5c66325815771a36de8c3
paine1690 <paine1690@126.com>
2016-10-07 16:36:24 +0800

    剑指offer： 21-23

--- src/codingInterviews/Chapter4.java
+++ src/codingInterviews/Chapter4.java
@@ -1,6 +1,8 @@
 package codingInterviews;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 /**
  * 		第四章 解决面试题的思路
@@ -67,16 +69,92 @@ public class Chapter4 {
     	return re;
     }
     
+    /*
+     * 22、栈的压入、弹出序列
+     */
+    public static boolean isPopOrder(int[] pushA, int[] popA){
+    	Stack<Integer> stack=new Stack<Integer>();
+    	int tar=0;
+    	int a=0;
+    	while(tar<popA.length){
+    		while(stack.isEmpty()||stack.peek()!=popA[tar]){
+    			if(a<pushA.length){
+    				stack.push(pushA[a++]);
+    			}else{
+    				return false;
+    			}    			
+    		}    		
+    		stack.pop();
+    		tar++;    		
+    	}
+    	return stack.isEmpty();
+    }
+    
+    /*
+     * 23、从上往下打印二叉树
+     */
+    public static ArrayList<Integer> printFromTopToBottom(TreeNode root){
+    	ArrayList<Integer> re=new ArrayList<Integer>();
+    	List<TreeNode> list=new ArrayList<TreeNode>();
+    	if(root==null){
+    		return re;
+    	}
+    	list.add(root);
+    	int cur=0, last=1;
+    	while(cur<list.size()){
+    		last=list.size();
+    		for(int i=cur; i<last; i++){
+    			TreeNode node=list.get(i);
+    			re.add(node.val);
+    			if(node.left!=null){
+    				list.add(node.left);
+    			}
+    			if(node.right!=null){
+    				list.add(node.right);
+    			}
+    		}
+    		cur=last;
+    	}    	
+    	return re;
+    }
     
 	public static void main(String[] args) {
-		int[][] matrix=new int[][]{
-			{1,2,3,4},
-			{5,6,7,8},
-			{9,10,11,12},
-			{13,14,15,16},
-			
-		};
-		System.out.println(printMatrix(matrix));
+		System.out.println(isPopOrder(new int[]{}, new int[]{}));
+//		int[][] matrix=new int[][]{
+//			{1,2,3,4},
+//			{5,6,7,8},
+//			{9,10,11,12},
+//			{13,14,15,16},
+//			
+//		};
+//		System.out.println(printMatrix(matrix));
 	}
-
 }
+
+/*
+ * 21、包含min函数的栈
+ */
+class minStack{
+	Stack<Integer> stack=new Stack<Integer>();
+	Stack<Integer> min=new Stack<Integer>();
+	
+	public int min(){
+		return min.peek();
+	}
+	
+	public void push(int val){
+		if(stack.isEmpty()||val<=min.peek()){
+			min.push(val);
+		}
+		stack.push(val);
+	}
+	
+	public int pop(){
+		int val=stack.pop();
+		if(val==min.peek()){
+			min.pop();
+		}
+		return val;
+	}
+}
+
