5734708a484922c77fb8586c16f6ab9a7ad598a7
paine1690 <paine1690@126.com>
2016-10-08 21:23:55 +0800

    ttt

---
+++ src/codingInterviews/Chapter3.java
@@ -0,0 +1,235 @@
+package codingInterviews;
+
+import java.util.Arrays;
+
+/**
+ * 		第三章 高质量的代码
+ * 
+ * 规范性
+ * 完整性    测试样例：功能测试，边界测试，负面测试
+ * 鲁棒性    链表或指针  多考虑null
+ * @author Paine
+ *
+ */
+public class Chapter3 {	
+	/*
+	 * 11、数值的整数次方
+	 */	
+	public static double pow(double x, int n){
+		if(n<0){
+			if(n==Integer.MIN_VALUE){
+				return 1/pow(x, Integer.MAX_VALUE)*x;
+			}else{
+				return 1/pow(x, -n);
+			}
+		}
+		if(n==0){
+			return 1.0;
+		}
+		double re=1.0;
+		while(n!=0){
+			if((n&1)==1){
+				re*=x;
+			}
+			x*=x;
+			n>>=1;
+		}
+		return re;		
+	}
+	
+	
+	/*
+	 * 12、 打印从1到最大的n位数
+	 * 注意数字溢出  数组 回溯
+	 */
+	private static void printNums(int[] nums){
+		int i=0;
+		while(i<nums.length&&nums[i]==0){
+			i++;
+		}
+		StringBuilder s=new StringBuilder();
+		for(; i<nums.length; i++){
+			s.append(nums[i]);
+		}		
+		System.out.println(s.toString());
+	}
+	
+	private static void dfsNums(int[] nums, int index){
+		if(index==nums.length){
+			printNums(nums);
+			return;
+		}
+		for(int i=0; i<10; i++){
+			nums[index]=i;
+			dfsNums(nums, index+1);
+		}		
+	}
+	
+	public static void printNums(int n){
+		int[] nums=new int[n];
+		dfsNums(nums, 0);
+		
+	}
+	
+	/*
+	 * 13、O(1)时间删除链表结点
+	 * 
+	 * leetcode 237. Delete Node in a Linked List
+	 */
+	
+	
+	/*
+	 * 14、 调整数组顺序使奇数位于偶数前面
+	 */
+	public static void adjust(int[] nums){
+		int i=0, j=nums.length-1;
+		while(i<=j){
+			if((nums[i]&1)==1){
+				i++;
+				continue;
+			}
+			if((nums[j]&1)!=1){
+				j--;
+				continue;
+			}
+			int temp=nums[i];
+			nums[i]=nums[j];
+			nums[j]=temp;
+			i++;
+			j--;
+		}
+		System.out.println(Arrays.toString(nums));
+	}
+	
+	/*
+	 * 15、链表中倒数第k个结点
+	 * 注意边界条件
+	 */
+	public static ListNode findKthToTail(ListNode head, int k){
+		if(head==null||k==0){
+			return null;
+		}
+		ListNode pNode=head;
+		while(--k>0){
+			pNode=pNode.next;
+			if(pNode==null){
+				return null;
+			}
+		}
+		
+		ListNode re=head;
+		while(pNode.next!=null){
+			pNode=pNode.next;
+			re=re.next;
+		}
+		return re;		
+	}
+	
+	/*
+	 * 16、反转链表
+	 */
+	public static ListNode reverseList(ListNode head){
+		if(head==null){
+			return head;
+		}
+		ListNode pNode=head, re=null;
+		while(pNode!=null){
+			ListNode next=pNode.next;
+			pNode.next=re;
+			re=pNode;
+			pNode=next;
+		}
+		return re;		
+	}
+	
+	/*
+	 * 17、合并两个排序的链表
+	 */
+	public static ListNode merge(ListNode list1, ListNode list2){
+		ListNode re=new ListNode(-1);
+		ListNode pNode=re;
+		while(list1!=null&&list2!=null){
+			if(list1.val<=list2.val){
+				pNode.next=list1;
+				pNode=list1;
+				list1=list1.next;				
+			}else{
+				pNode.next=list2;
+				pNode=list2;
+				list2=list2.next;
+			}
+		}
+		if(list1!=null){
+			pNode.next=list1;
+		}
+		if(list2!=null){
+			pNode.next=list2;
+		}
+		
+		return re.next;
+	}
+	
+	/*
+	 * 18、树的子结构
+	 */
+	private static boolean isSame(TreeNode root1, TreeNode root2){
+		if(root2==null){
+			return true;
+		}
+		if(root1==null){
+			return false;
+		}
+		if(root1.val==root2.val){
+			return isSame(root1.left, root2.left)&&isSame(root1.right, root2.right);
+		}
+		return false;		
+	}
+	
+	public static boolean subTree(TreeNode root1, TreeNode root2){
+		if(root1==null||root2==null){
+			return false;
+		}
+		if(root1.val==root2.val&&isSame(root1, root2)){
+			return true;
+		}
+		return subTree(root1.left, root2)||subTree(root1.right, root2);
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		printNums(3);
+		//System.out.println(pow(0, -5));
+	}
+
+}
+
+class ListNode{
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/codingInterviews/Chapter4.java
@@ -0,0 +1,331 @@
+package codingInterviews;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
+
+/**
+ * 		第四章 解决面试题的思路
+ * 
+ * @author Paine
+ *
+ */
+public class Chapter4 {
+	
+	/*
+	 * 19、二叉树镜像
+	 */
+	public static void mirror(TreeNode root){
+		if(root==null){
+			return;
+		}
+		TreeNode temp=root.left;
+		root.left=root.right;
+		root.right=temp;
+		mirror(root.left);
+		mirror(root.right);
+	}
+	
+	/*
+	 * 20、顺时针打印矩阵
+	 */
+	private static void printCircle(ArrayList<Integer> re, int[][] matrix, int m, int n, int start){
+		int endJ=n-start-1;
+		int endI=m-start-1;
+		
+		for(int j=start; j<=endJ; j++){
+			re.add(matrix[start][j]);
+		}
+		
+		if(start<endI){
+			for(int i=start+1; i<=endI; i++){
+				re.add(matrix[i][endJ]);
+			}
+		}
+		
+		if(start<endI&&start<endJ){
+			for(int j=endJ-1; j>=start; j--){
+				re.add(matrix[endI][j]);
+			}
+		}		
+		
+		if(start<endI-1&&start<endJ){
+			for(int i=endI-1; i>start; i--){
+				re.add(matrix[i][start]);
+			}
+		}	
+	}
+	
+    public static ArrayList<Integer> printMatrix(int [][] matrix) {
+    	ArrayList<Integer> re=new ArrayList<Integer>();
+    	if(matrix.length==0||matrix[0].length==0){
+    		return re;
+    	}
+    	int m=matrix.length;
+    	int n=matrix[0].length;
+    	for(int start=0; start*2<Math.min(m,  n); start++){
+    		printCircle(re, matrix, m, n, start);
+    	}
+    	return re;
+    }
+    
+    /*
+     * 22、栈的压入、弹出序列
+     */
+    public static boolean isPopOrder(int[] pushA, int[] popA){
+    	Stack<Integer> stack=new Stack<Integer>();
+    	int tar=0;
+    	int a=0;
+    	while(tar<popA.length){
+    		while(stack.isEmpty()||stack.peek()!=popA[tar]){
+    			if(a<pushA.length){
+    				stack.push(pushA[a++]);
+    			}else{
+    				return false;
+    			}    			
+    		}    		
+    		stack.pop();
+    		tar++;    		
+    	}
+    	return stack.isEmpty();
+    }
+    
+    /*
+     * 23、从上往下打印二叉树
+     */
+    public static ArrayList<Integer> printFromTopToBottom(TreeNode root){
+    	ArrayList<Integer> re=new ArrayList<Integer>();
+    	List<TreeNode> list=new ArrayList<TreeNode>();
+    	if(root==null){
+    		return re;
+    	}
+    	list.add(root);
+    	int cur=0, last=1;
+    	while(cur<list.size()){
+    		last=list.size();
+    		for(int i=cur; i<last; i++){
+    			TreeNode node=list.get(i);
+    			re.add(node.val);
+    			if(node.left!=null){
+    				list.add(node.left);
+    			}
+    			if(node.right!=null){
+    				list.add(node.right);
+    			}
+    		}
+    		cur=last;
+    	}    	
+    	return re;
+    }
+    
+    /*
+     * 24、二叉搜索树的后续遍历序列
+     */
+    private static boolean isBST(int[] nums, int start, int end){
+    	if(start>=end){
+    		return true;
+    	}
+    	int mid=start;
+    	while(nums[mid]<nums[end]){
+    		mid++;
+    	}
+    	for(int i=mid; i<end; i++){
+    		if(nums[i]<nums[end]){
+    			return false;
+    		}
+    	}
+    	return isBST(nums, start, mid-1)&&isBST(nums, mid, end-1);    	
+    }
+    
+    
+    public static boolean verifySquenceOfBST(int [] sequence) {
+        if(sequence.length==0){
+        	return false;
+        }    
+    	return isBST(sequence, 0, sequence.length-1);
+    }
+    
+    /*
+     * 25、二叉树中和为某一值的路径
+     */
+    private static void find(ArrayList<ArrayList<Integer>> re, ArrayList<Integer> list, int target, TreeNode root, List<TreeNode> temp){
+    	temp.add(root);
+    	list.add(root.val);
+    	if(root.left==null&&root.right==null){
+    		if(target==root.val){
+    			re.add(new ArrayList<Integer>(list));
+    		}
+    		return;
+    	}    	
+    	if(root.left!=null){
+    		find(re, list, target-root.val, root.left, temp);
+    		temp.remove(temp.size()-1);
+    		list.remove(list.size()-1);
+    	}
+    	if(root.right!=null){
+    		find(re, list, target-root.val, root.right, temp);
+    		temp.remove(temp.size()-1);
+    		list.remove(list.size()-1);
+    	}
+    }
+    
+    public static ArrayList<ArrayList<Integer>> findPath(TreeNode root,int target) {
+    	ArrayList<ArrayList<Integer>> re=new ArrayList<ArrayList<Integer>>();
+    	if(root==null){
+    		return re; 
+    	}
+    	ArrayList<Integer> list=new ArrayList<Integer>();
+    	List<TreeNode> temp=new ArrayList<TreeNode>();
+    	find(re, list, target, root, temp);
+    	return re;
+    }
+    
+    /*
+     * 26、复杂链表的复制
+     */
+    public RandomListNode Clone(RandomListNode pHead){
+        if(pHead==null){
+        	return null;
+        }
+        RandomListNode pNode=pHead;
+        while(pNode!=null){
+        	RandomListNode next=pNode.next;
+        	RandomListNode copyNode=new RandomListNode(pNode.label);
+        	pNode.next=copyNode;
+        	copyNode.next=next;
+        	pNode=next;
+        }
+        
+        pNode =pHead;
+        RandomListNode copyNode;
+        while(pNode!=null){
+        	copyNode=pNode.next;
+        	if(pNode.random!=null){
+        		copyNode.random=pNode.random.next;
+        	}
+        	pNode=copyNode.next;
+        }
+        
+        RandomListNode re=pHead.next;
+        pNode=pHead;
+        while(pNode.next!=null){
+        	copyNode=pNode.next;
+        	pNode.next=copyNode.next;
+        	pNode=copyNode;
+        }
+        return re;
+    }
+    
+    /*
+     * 27、二叉搜索树与双向链表
+     */
+    TreeNode last=null;
+    private void dfs(TreeNode root){
+    	if(root==null){
+    		return;
+    	}
+    	
+    	dfs(root.left);    	
+    	root.left=last;
+    	if(last!=null){
+    		last.right=root;
+    	}
+    	last=root;
+    	dfs(root.right);
+    }
+    
+    public TreeNode Convert(TreeNode pRootOfTree) {
+    	dfs(pRootOfTree);   
+    	while(last!=null&&last.left!=null){
+    		last=last.left;
+    	}
+    	return last;
+    }
+    
+    /*
+     * 28、字符串的排列
+     */
+    private static void swap(char[] chars, int i, int j){
+    	if(i!=j){
+    		char temp=chars[i];
+    		chars[i]=chars[j];
+    		chars[j]=temp;
+    	}
+    }
+    
+    private static void dfsGet(ArrayList<String> re, char[] chars, int start, int end){
+    	if(start==end){
+    		re.add(new String(chars));
+    	}
+    	
+    	for(int i=start; i<=end; i++){
+    		if(i!=start&&chars[i]==chars[start]){
+    			continue;
+    		}
+    		swap(chars, start, i);
+    		dfsGet(re, chars, start+1, end);
+    		swap(chars, start, i);
+    	}
+    }
+    
+    public static ArrayList<String> Permutation(String str) {
+        ArrayList<String> re=new ArrayList<String>();
+        char[] chars=str.toCharArray();
+        Arrays.sort(chars);
+        dfsGet(re, chars, 0, chars.length-1);
+        Collections.sort(re);
+        return re;
+    }
+    
+	public static void main(String[] args) {
+		System.out.println(Permutation("aabc"));
+//		int[][] matrix=new int[][]{
+//			{1,2,3,4},
+//			{5,6,7,8},
+//			{9,10,11,12},
+//			{13,14,15,16},
+//			
+//		};
+//		System.out.println(printMatrix(matrix));
+	}
+}
+
+/*
+ * 21、包含min函数的栈
+ */
+class minStack{
+	Stack<Integer> stack=new Stack<Integer>();
+	Stack<Integer> min=new Stack<Integer>();
+	
+	public int min(){
+		return min.peek();
+	}
+	
+	public void push(int val){
+		if(stack.isEmpty()||val<=min.peek()){
+			min.push(val);
+		}
+		stack.push(val);
+	}
+	
+	public int pop(){
+		int val=stack.pop();
+		if(val==min.peek()){
+			min.pop();
+		}
+		return val;
+	}
+}
+
+class RandomListNode {
+    int label;
+    RandomListNode next = null;
+    RandomListNode random = null;
+
+    RandomListNode(int label) {
+        this.label = label;
+    }
+}
