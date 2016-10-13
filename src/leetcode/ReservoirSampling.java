40a3a23f87804a423003d0f523ce44f256591e4a
paine1690 <paine1690@126.com>
2016-10-13 19:28:22 +0800

    398. Random Pick Index

--- src/leetcode/ReservoirSampling.java
+++ src/leetcode/ReservoirSampling.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 /**
@@ -11,34 +13,70 @@ public class ReservoirSampling {
 
 	
 	//382. Linked List Random Node
-	ListNode head;
-	
-    public ReservoirSampling(ListNode head) {
-        this.head=head;        
-    }
+	class Solution{
+		ListNode head;
+		
+	    public Solution(ListNode head) {
+	        this.head=head;        
+	    }
+	    
+	    /** Returns a random node's value. */
+	    public int getRandom() {
+	    	ListNode head=this.head;
+	    	Random r=new Random();
+	    	int re=head.val;
+	    	int i=1;
+	        while(head.next!=null){
+	        	head=head.next;        	
+	        	int m=r.nextInt(i+1);
+	        	if(m==0){
+	        		re=head.val;
+	        	}
+	        	i++;
+	        }
+	        return re;
+	    }
+	}
+
+	//398. Random Pick Index
+	class Solution2 {
+		int[] nums;
+		Map<Integer, Integer> map=new HashMap<Integer, Integer>();
+		Random r=new Random();
+		
+	    public Solution2(int[] nums) {
+	        this.nums=nums;
+	        for(int i=0; i<nums.length; i++){
+	        	if(map.containsKey(nums[i])){
+	        		map.put(nums[i], map.get(nums[i])+1);
+	        	}else{
+	        		map.put(nums[i], 1);
+	        	}
+	        }
+	    }
+	    
+	    public int pick(int target) {
+	        int len=map.get(target);
+	        int m=r.nextInt(len);
+	        int i=0;
+	        for(i=0; i<nums.length; i++){
+	        	if(nums[i]==target){
+	        		if(m==0){
+	        			break;
+	        		}
+	        		else{
+	        			m--;
+	        		}
+	        	}
+	        }	
+	        return i;
+	    }
+	}
+
     
-    /** Returns a random node's value. */
-    public int getRandom() {
-    	ListNode head=this.head;
-    	Random r=new Random();
-    	int re=head.val;
-    	int i=1;
-        while(head.next!=null){
-        	head=head.next;        	
-        	int m=r.nextInt(i+1);
-        	if(m==0){
-        		re=head.val;
-        	}
-        	i++;
-        }
-        return re;
-    }
     
     
-    
-    
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {		
 
 	}
 
