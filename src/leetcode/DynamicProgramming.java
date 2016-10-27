86fbd5c05b8dda4404951823299110b59090beb9
paine1690 <paine1690@126.com>
2016-10-27 14:29:41 +0800

    44. Wildcard Matching dp

--- src/leetcode/DynamicProgramming.java
+++ src/leetcode/DynamicProgramming.java
@@ -614,9 +614,26 @@ public class DynamicProgramming {
     	return dp[m][n];
     }
     
-    
+    //44. Wildcard Matching
+    public static boolean isMatch2(String s, String p) {
+    	int m=p.length(), n=s.length();
+    	boolean[][] dp=new boolean[m+1][n+1];
+    	dp[0][0]=true;
+    	for(int i=1; i<=m; i++){
+    		dp[i][0]=dp[i-1][0]&&p.charAt(i-1)=='*';
+    		for(int j=1; j<=n; j++){
+    			if(p.charAt(i-1)=='*'){
+    				dp[i][j]=dp[i-1][j]||dp[i][j-1]||dp[i-1][j-1];
+    			}else{
+    				dp[i][j]=(p.charAt(i-1)=='?'||p.charAt(i-1)==s.charAt(j-1))&&dp[i-1][j-1];
+    			}
+    		}
+    	}
+    	
+    	return dp[m][n];
+    }
 	public static void main(String[] args) {
-		System.out.println(isMatch("aab","c*a*b"));
+		System.out.println(isMatch2("","*"));
 		//System.out.println(wiggleMaxLength(new int[]{1,2,3}));
 		//System.out.println(isSubsequence("ac", "ajibjic"));
 //		int nums[]={1,3,6,7,9,4,10,5,6};
