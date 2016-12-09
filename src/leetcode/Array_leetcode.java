6f7296d9020e78efa5430f585b6580cceb14f937
paine1690 <paine1690@126.com>
2016-12-09 11:03:16 +0800

    419. Battleships in a Board

--- src/leetcode/Array_leetcode.java
+++ src/leetcode/Array_leetcode.java
@@ -523,7 +523,24 @@ public class Array_leetcode {
         return re;
     }
     
-    
+    //419. Battleships in a Board
+    public int countBattleships(char[][] board) {
+        if(board.length==0||board[0].length==0){
+        	return 0;
+        }
+        int re=0, m=board.length, n=board[0].length;
+        for(int i=0; i<m; i++){
+        	for(int j=0; j<n; j++){
+        		if(board[i][j]=='X'){
+        			if((i>0&&board[i-1][j]=='X')||(j>0&&board[i][j-1]=='X')){
+        			}else{
+        				re++;
+        			}
+        		}
+        	}
+        }
+        return re;
+    }
     
 	public static void main(String[] args) {		
 		
