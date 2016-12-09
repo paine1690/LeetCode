d5d2ba080d952e9bc93c7d0ae81e00dae46d7eab
paine1690 <paine1690@126.com>
2016-12-09 10:39:11 +0800

    452. Minimum Number of Arrows to Burst Balloons

--- src/leetcode/Greedy.java
+++ src/leetcode/Greedy.java
@@ -1,6 +1,7 @@
 package leetcode;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Greedy {
 
@@ -64,11 +65,84 @@ public class Greedy {
     	return re;    	
     }
     
+    //435. Non-overlapping Intervals
+    static class Interval {
+    	int start;
+    	int end;
+    	Interval() { start = 0; end = 0; }
+    	Interval(int s, int e) { start = s; end = e; }
+    }
     
+    public int eraseOverlapIntervals(Interval[] intervals) {
+        if(intervals.length<2){
+        	return 0;
+        }
+        Arrays.sort(intervals, new Comparator<Interval>(){
+
+			@Override
+			public int compare(Interval i1, Interval i2) {
+				if(i1.start!=i2.start){
+					return i1.start>i2.start? 1: -1;
+				}else{
+					return i1.end==i2.end? 0: i1.end>i2.end? 1: -1;
+				}
+			}
+        	
+        });
+    	int re=0, max=intervals[0].end;
+    	for(int i=1; i<intervals.length; i++){
+    		if(intervals[i].start==intervals[i-1].start){
+    			re++;
+    		}else{
+    			if(intervals[i].start<max){
+    				re++;
+    				max=Math.min(max, intervals[i].end);
+    			}else{
+    				max=intervals[i].end;
+    			}
+    			
+    		}
+    	}    	
+    	return re;
+    }
+    
+    //452. Minimum Number of Arrows to Burst Balloons
+    public static int findMinArrowShots(int[][] points) {
+    	if(points.length<2){
+    		return points.length;
+    	}
+    	int re=1;
+    	Arrays.sort(points, new Comparator<int[]>(){
+			@Override
+			public int compare(int[] i1, int[] i2) {
+				if(i1[0]==i2[0]){
+					return i1[1]==i2[1]? 0: i1[1]>i2[1]? 1: -1;
+				}else{
+					return i1[0]>i2[0]? 1: -1;
+				}
+			}
+    	});
+    	
+    	int pre=points[0][1];
+    	for(int i=1; i<points.length; i++){
+    		if(points[i][0]==points[i-1][0]){
+    			continue;
+    		}    		
+    		if(points[i][0]<=pre){
+    			pre=Math.min(pre, points[i][1]);
+    		}else{
+    			re++;
+    			pre=points[i][1];
+    		}
+    	}
+    	return re;
+    }
     
     
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(findMinArrowShots(new int[][]{
+			{3,9},{7,12},{3,8},{6,8},{9,10},{2,9},{0,9},{3,9},{0,6},{2,8}
+		}));
 
 	}
 
