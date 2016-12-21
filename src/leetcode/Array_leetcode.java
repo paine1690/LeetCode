1c042656ec7703d6018c6e9bed3eeea9d9db5d9d
paine1690 <paine1690@126.com>
2016-12-21 11:27:12 +0800

    228. Summary Ranges

--- src/leetcode/Array_leetcode.java
+++ src/leetcode/Array_leetcode.java
@@ -662,8 +662,37 @@ public class Array_leetcode {
     	return re;
     }
     
+    //228. Summary Ranges
+    public static List<String> summaryRanges(int[] nums) {
+        List<String> re=new ArrayList<String>();
+        if(nums.length==0){
+        	return re;
+        }
+        int start=nums[0];
+        for(int i=1; i<nums.length; i++){
+        	if(nums[i]!=nums[i-1]+1){
+        		int end=nums[i-1];
+        		if(start==end){
+        			re.add(String.valueOf(start));
+        		}else{
+        			re.add(start+"->"+end);
+        		}
+        		start=nums[i];
+        	}
+        }
+        int end=nums[nums.length-1];
+		if(start==end){
+			re.add(String.valueOf(start));
+		}else{
+			re.add(start+"->"+end);
+		}
+        
+        return re;
+    }
+    
+    
 	public static void main(String[] args) {		
-		System.out.println(generateMatrix(4));
+		System.out.println(summaryRanges(new int[]{0,1,2,4,5,7}));
 		
 
 	}
