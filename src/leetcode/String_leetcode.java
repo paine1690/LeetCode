c21d29d8296cb5d3f48fb6f1205448aa44c07dc7
Paine <paine1690@126.com>
2017-08-07 18:17:12 +0800

    583. Delete Operation for Two Strings

--- src/leetcode/String_leetcode.java
+++ src/leetcode/String_leetcode.java
@@ -975,7 +975,22 @@ public class String_leetcode {
     	return (a1*b1-a2*b2)+"+"+(a1*b2+a2*b1)+"i";    	
     }    
     
-	public static void main(String[] args) {
-		System.out.println(complexNumberMultiply("1+1i", "1+1i"));
-	}
+    //583. Delete Operation for Two Strings
+    public static int minDistance(String word1, String word2) {
+      int m = word1.length(), n = word2.length();
+      int[][] dp = new int[m + 1][n + 1];
+      
+      for (int i = 1; i <=m; i++) {
+        for (int j = 1; j <= n; j++) {
+          if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+            dp[i][j] = dp[i - 1][j - 1] + 1;
+          } else {
+            dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+          }
+        }
+      }
+      return m + n - dp[m][n] * 2;
+    }
+    
+
 }
